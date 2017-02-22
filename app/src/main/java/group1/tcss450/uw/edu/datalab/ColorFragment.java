1a0ebaa2c53f4d3bc4745df13544e0e9da1dac49
kevin <shw26@uw.edu>
2017-02-22 03:08:27 -0800

    Initial commit

---
+++ app/src/main/java/group1/tcss450/uw/edu/datalab/ColorFragment.java
@@ -0,0 +1,122 @@
+package group1.tcss450.uw.edu.datalab;
+
+import android.content.Context;
+import android.graphics.drawable.GradientDrawable;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link ColorFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ */
+public class ColorFragment extends Fragment implements AdapterView.OnItemSelectedListener{
+
+    private OnFragmentInteractionListener mListener;
+    Spinner spinner;
+
+    public ColorFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_color, container, false);
+        spinner = (Spinner) v.findViewById(R.id.spinner);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(),
+                R.array.auto_complete_colors, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+
+        return v;
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(int color, int position) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(color, position);
+        }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        int arg;
+        if (getArguments() != null){
+
+             arg = getArguments().getInt(getString(R.string.POSITION));
+        }else {
+            arg = 0;
+        }
+        spinner.setSelection(arg);
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String color = (String) parent.getAdapter().getItem(position);
+        Toast.makeText(getActivity(),
+                "The color is " + color,
+                Toast.LENGTH_SHORT).show();
+
+        int[] colorArr = this.getResources().getIntArray(R.array.colorArray);
+
+        getActivity().findViewById(R.id.content_main).setBackgroundColor(colorArr[position]);
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(int color, int position);
+    }
+}
---
+++ app/src/main/java/group1/tcss450/uw/edu/datalab/MainActivity.java
@@ -0,0 +1,110 @@
+package group1.tcss450.uw.edu.datalab;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.io.OutputStreamWriter;
+
+public class MainActivity extends AppCompatActivity implements ColorFragment.OnFragmentInteractionListener{
+
+    private SharedPreferences mPrefs;
+    ColorFragment mColorFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        mColorFragment = new ColorFragment();
+
+        mPrefs = getSharedPreferences(getString(R.string.SHARED_PREFS), Context.MODE_PRIVATE);
+        findViewById(R.id.content_main).
+                setBackgroundColor(mPrefs.getInt(getString(R.string.COLOR), Color.RED));
+        int pos = mPrefs.getInt(getString(R.string.POSITION), 0);
+
+        if (savedInstanceState == null) {
+            if (findViewById(R.id.content_main) != null) {
+                mColorFragment = new ColorFragment();
+                if (pos != 0) {
+                    Bundle args = new Bundle();
+                    args.putInt(getString(R.string.POSITION), pos);
+                    mColorFragment.setArguments(args);
+                }
+
+                getSupportFragmentManager().beginTransaction()
+                        .add(R.id.content_main, mColorFragment)
+                        .commit();
+            }
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.file_menu_item) {
+            // TODO: 2017/2/22  something happened over here. mColorFragment.getArguments() is null.
+            mColorFragment.getArguments().putInt(getString(R.string.POSITION),
+                    mPrefs.getInt(getString(R.string.POSITION), 0));
+
+            FragmentTransaction transaction = getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.content_main, new FileFragment())
+                    .addToBackStack(null);
+            // Commit the transaction
+            transaction.commit();
+        }
+
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onFragmentInteraction(int color, int pos) {
+        saveToSharedPrefs(color, pos);
+        saveToFile(color, pos);
+    }
+
+    private void saveToSharedPrefs(int color, int pos) {
+        mPrefs.edit().putInt(getString(R.string.COLOR), color).apply();
+        mPrefs.edit().putInt(getString(R.string.POSITION), pos).apply();
+    }
+
+    private void saveToFile(int color, int pos) {
+        try {
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(
+                    openFileOutput(getString(R.string.COLOR)
+                            , Context.MODE_APPEND));
+            outputStreamWriter.append("color = r:" );
+            outputStreamWriter.append(Color.red(color) + ", g:");
+            outputStreamWriter.append(Color.green(color) + ", b:");
+            outputStreamWriter.append(Color.blue(color) + ", a:");
+            outputStreamWriter.append(Color.alpha(color) + "\n");
+        outputStreamWriter.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
